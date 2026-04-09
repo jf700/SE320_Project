f99a20e75a42607c0488ae80403b315dec6fbb3a
Josh Fuery <jfuery@chapman.edu>
2026-04-08 20:12:51 -0700

    feat: add CLI runner on startup, split C4 component diagrams, remove AUTO_SERVER from H2 URL

--- src/main/java/com/digitaltherapy/DigitalTherapyAssistantApplication.java
+++ src/main/java/com/digitaltherapy/DigitalTherapyAssistantApplication.java
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import com.digitaltherapy.cli.DigitalTherapyCLIClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @OpenAPIDefinition(
@@ -23,5 +27,15 @@ public class DigitalTherapyAssistantApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DigitalTherapyAssistantApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner cliRunner(DigitalTherapyCLIClient cliClient,
+                                @Value("${app.cli.enabled:false}") boolean cliEnabled) {
+        return args -> {
+            if (cliEnabled) {
+                cliClient.start();
+            }
+        };
     }
 }
